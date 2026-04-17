4f4bec946192a6eaa6d60a42012256c18a996779
Nuziatella <67765305+Nuziatella@users.noreply.github.com>
2026-04-17 14:17:49 -0400

    Release v1.4.5

--- main.lua
+++ main.lua
@@ -51,7 +51,7 @@ local Ui = modules.ui
 local addon = {
     name = Constants ~= nil and Constants.ADDON_NAME or "Nuzi Fishing",
     author = Constants ~= nil and Constants.ADDON_AUTHOR or "Nuzi",
-    version = Constants ~= nil and Constants.ADDON_VERSION or "1.4.0",
+    version = Constants ~= nil and Constants.ADDON_VERSION or "1.4.5",
     desc = Constants ~= nil and Constants.ADDON_DESC or "Fishing coach HUD"
 }
 
